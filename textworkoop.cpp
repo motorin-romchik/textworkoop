0479b5b11d4c5b60a12355484d1f9b32a9f4b8f7
mr_str <80831520+motorin-romchik@users.noreply.github.com>
2022-02-08 23:20:30 +0700

    Добавьте файлы проекта.

---
+++ textworkoop.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+/* 
+* сохранение данных в файл
+*/
+int main(void)
+{
+	setlocale(LC_ALL, "ru");
+
+	string path;
+	cin >> path;
+	
+	ofstream fout;
+	fout.open(path);
+
+	if (!fout.is_open()) {
+		cout << "Ошибка открытия файла!" << endl;
+	}
+	else 
+	{
+		fout << "Это наши данные dblfkb xj!"<<endl;
+		fout << "Vjnjhby Hjvfy";
+	}
+	fout.close();
+}
